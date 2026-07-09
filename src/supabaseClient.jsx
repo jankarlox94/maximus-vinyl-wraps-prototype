ed9ba4d970a601691430205f97b91879e88eb205
jankarlox94 <giancarlosanchez.dev@icloud.com>
2026-07-09 18:56:18 -0500

    Updated favicon and app title

--- src/supabaseClient.jsx
+++ src/supabaseClient.jsx
@@ -2,7 +2,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Retrieve the environment variables
-// Use process.env.REACT_APP_... if you are using Create React App instead of Vite
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
